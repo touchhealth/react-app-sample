37fd83d3a2ea28c2998db7d5fbfd8aa551cd6b7b
kasten <kasten@touchtec.com.br>
2018-11-09 17:12:06 -0200

    Configurando loader de svgx

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,6 +1,8 @@
 import '@babel/polyfill'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import Logo from './LogoTouchHealth.svgx'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 
@@ -10,6 +12,7 @@ import Tab from '@touchhealth/react-components-sample/TabThemed'
 ReactDOM.render(
 <Theme theme={{primaryColor: 'red'}}>
     <div style={{padding: '20px'}}>
+        <Logo width={150} height={40}></Logo>
         <h1>Teste da App React</h1>
         <Tab
             titles={['Pag1', 'Pag2']}
--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,10 @@ module.exports = {
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [ 'file-loader' ]
+            },
+            {
+                test: /\.(svgx)$/,
+                use: [ 'react-svg-loader' ]
             }
         ]
     },
